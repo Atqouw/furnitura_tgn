9ac899c51f339dcc07afa279bfc062be80747769
Александр Дьяченко <samoe.vajnoe@mail.ru>
2019-01-05 21:52:04 +0300

    Added remove item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,6 +13,14 @@ class ItemsController < WebApplicationController
     @item = Item.new
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def destroy
+    @item.destroy
+  end
+
   def update
     @item = Item.find(params[:id])
     if @item.update_attributes(item_params)
